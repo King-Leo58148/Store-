6c7c56a43d99dfb1137fdefb7075d16629aca2dc
Louis Sanyenni <lsanyenni@gmail.com>
2026-05-01 11:04:55 +0000

    depency injections

---
+++ src/main/java/com/codewithlouis/store/Dev.java
@@ -0,0 +1,9 @@
+package com.codewithlouis.store;
+
+public class Dev {
+
+    public void build(){
+        System.out.println("Working on Awesome Project");
+
+    }
+}
--- src/main/java/com/codewithlouis/store/StoreApplication.java
+++ src/main/java/com/codewithlouis/store/StoreApplication.java
@@ -8,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-	ApplicationContext context= SpringApplication.run(StoreApplication.class, args);
-
+	SpringApplication.run(StoreApplication.class, args);
+	 Dev obj = new Dev();
+     obj.build();
 	}
 
 }
