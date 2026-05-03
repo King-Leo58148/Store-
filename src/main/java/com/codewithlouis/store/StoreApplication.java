c8ec9f81f8c6918bf92e765f5c17c12035df053d
Louis Sanyenni <lsanyenni@gmail.com>
2026-05-03 20:18:22 +0000

    commit

--- src/main/java/com/codewithlouis/store/StoreApplication.java
+++ src/main/java/com/codewithlouis/store/StoreApplication.java
@@ -3,7 +3,6 @@ package com.codewithlouis.store;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class StoreApplication {
 //
