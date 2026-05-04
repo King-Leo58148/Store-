3c41fab288563f289f087fadfb76d8ca05d3b76c
Louis Sanyenni <lsanyenni@gmail.com>
2026-05-04 14:47:38 +0000

    Types of injections

---
+++ src/main/java/com/codewithlouis/store/Laptop.java
@@ -0,0 +1,12 @@
+package com.codewithlouis.store;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Laptop {
+    public void compile(){
+
+        System.out.println("Compiling with 404 bugs");
+    }
+}
